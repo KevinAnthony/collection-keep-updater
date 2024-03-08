204b3df3c7c61eacf15459b27c7fed2839ff78fb
Kevin Anthony <kevin.s.anthony@gmail.com>
2024-03-08 10:14:25 -0500

    autoformat

--- types/isbn_books.go
+++ types/isbn_books.go
@@ -1,8 +1,9 @@
 package types
 
 import (
-	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/kevinanthony/collection-keep-updater/out"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 )
 
